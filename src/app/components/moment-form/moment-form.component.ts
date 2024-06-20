40d1197754c4484f0ae6d27cf939a617d49dd6d9
Wendel Segadilha <wendelsegadilha99@gmail.com>
2024-06-20 08:56:32 -0300

    Iniciando componente de mensagem

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Moment } from '../../interfaces/Moment';
+import { Moment } from '../../interfaces/moment';
 
 @Component({
   selector: 'app-moment-form',
